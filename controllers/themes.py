e22b3469df2130b50914e9054f766a3932162c74
cwen0708 <cwen0708@gmail.com>
2016-12-16 14:28:50 +0800

    字串一律使用單引號

--- controllers/themes.py
+++ controllers/themes.py
@@ -16,42 +16,42 @@ import os
 
 class Themes(Controller):
     class Meta:
-        pagination_actions = ("list", "pickup_list",)
+        pagination_actions = ('list', 'pickup_list',)
         pagination_limit = 10
         
     class Scaffold:
-        display_properties_in_list = ("theme_name", "theme_name")
+        display_properties_in_list = ('theme_name', 'theme_name')
 
     @route
     def admin_upload(self):
         self.meta.change_view('json')
-        namespace_manager.set_namespace("shared")
-        theme_title = self.params.get_string("theme_title", '')
-        theme_name = self.params.get_string("theme_name", '')
-        exclusive = self.params.get_string("exclusive", self.namespace)
-        author = self.params.get_string("author", '')
-        thumbnail = self.params.get_string("thumbnail", '/assets/themes/%s/img/theme.png' % theme_name)
-        using = self.params.get_string("using", '')
+        namespace_manager.set_namespace('shared')
+        theme_title = self.params.get_string('theme_title', '')
+        theme_name = self.params.get_string('theme_name', '')
+        exclusive = self.params.get_string('exclusive', self.namespace)
+        author = self.params.get_string('author', '')
+        thumbnail = self.params.get_string('thumbnail', '/assets/themes/%s/img/theme.png' % theme_name)
+        using = self.params.get_string('using', '')
         model = self.meta.Model
         is_find = model.check_in_list(self.namespace, theme_name=theme_name)
-        if thumbnail.startswith("/"):
+        if thumbnail.startswith('/'):
             thumbnail = thumbnail[1:]
-        if thumbnail.startswith("themes"):
-            thumbnail = "assets/" + thumbnail
-        if thumbnail.startswith("assets/themes") is False:
+        if thumbnail.startswith('themes'):
+            thumbnail = 'assets/' + thumbnail
+        if thumbnail.startswith('assets/themes') is False:
             thumbnail = 'assets/themes/%s/%s' % (theme_name, thumbnail)
-        thumbnail = "/" + thumbnail
+        thumbnail = '/' + thumbnail
         self.logging(thumbnail)
         if is_find:
             self.context['data'] = {
-                'info': "done",
-                "theme": theme_name
+                'info': 'done',
+                'theme': theme_name
             }
             n = model.find_by_theme_name(theme_name)
         else:
             self.context['data'] = {
-                'info': "create",
-                "theme": theme_name
+                'info': 'create',
+                'theme': theme_name
             }
             n = model()
         n.theme_title = theme_title
@@ -66,52 +66,52 @@ class Themes(Controller):
     @route_with('/admin/themes/set.json')
     def admin_get_url(self):
         self.meta.change_view('json')
-        namespace_manager.set_namespace("shared")
-        theme_name = self.params.get_string("theme_name", '')
+        namespace_manager.set_namespace('shared')
+        theme_name = self.params.get_string('theme_name', '')
         model = self.meta.Model
         is_find = model.check_in_list(self.namespace, theme_name=theme_name)
         if is_find:
             self.settings.set_theme(self.server_name, self.namespace, theme_name)
             self.context['data'] = {
-                'info': "done",
-                "theme": theme_name
+                'info': 'done',
+                'theme': theme_name
             }
         else:
             self.context['data'] = {
-                'info': "not_in_list"
+                'info': 'not_in_list'
             }
 
     @route
-    @route_menu(list_name=u"backend", text=u"主題樣式", sort=9600, group=u"視覺形象", need_hr_parent=True)
+    @route_menu(list_name=u'backend', text=u'主題樣式', sort=9600, group=u'視覺形象', need_hr_parent=True)
     def admin_pickup_list(self):
-        self.context["current_theme"] = self.theme
+        self.context['current_theme'] = self.theme
         self.meta.pagination_limit = 100
         theme_list = self.get_themes_list(self)
         model = self.meta.Model
 
         def query_factory_with_identifier(self):
             return model.query(
-                model.exclusive.IN([self.namespace, u"all"])
+                model.exclusive.IN([self.namespace, u'all'])
             ).order(-model.sort, -model._key)
         self.scaffold.query_factory = query_factory_with_identifier
-        namespace_manager.set_namespace("shared")
+        namespace_manager.set_namespace('shared')
         scaffold.list(self)
-        self.context["new_item_list"] = []
+        self.context['new_item_list'] = []
         for item_p in theme_list:
             is_not_find = True
             for item_d in self.context[self.scaffold.plural]:
-                if item_d.theme_name == item_p["theme_name"]:
+                if item_d.theme_name == item_p['theme_name']:
                     is_not_find = False
             if is_not_find:
                 n = self.meta.Model()
-                n.theme_title = item_p["theme_title"]
-                n.theme_name = item_p["theme_name"]
-                n.exclusive = item_p["exclusive"]
-                n.author = item_p["author"]
-                n.using = json.dumps(item_p["using"])
+                n.theme_title = item_p['theme_title']
+                n.theme_name = item_p['theme_name']
+                n.exclusive = item_p['exclusive']
+                n.author = item_p['author']
+                n.using = json.dumps(item_p['using'])
                 n.in_datastore = False
                 n.put()
-                self.context["new_item_list"].append(n)
+                self.context['new_item_list'].append(n)
 
     @staticmethod
     def get_themes_list(self, other=None):
@@ -126,33 +126,33 @@ class Themes(Controller):
             except:
                 pass
             for dirPath in dirs:
-                if dirPath.find(".") >= 0:
+                if dirPath.find('.') >= 0:
                     continue
-                file_path = os.path.join(themes_dir, dirPath, "theme.json")
+                file_path = os.path.join(themes_dir, dirPath, 'theme.json')
                 if os.path.exists(file_path) is False:
                     continue
                 f = open(file_path, 'r')
                 data = json.load(f)
                 thumbnail = '/assets/themes/%s/img/theme.png' % dirPath
-                if "thumbnail" in data:
-                    thumbnail = data["thumbnail"]
-                if thumbnail.startswith("/"):
+                if 'thumbnail' in data:
+                    thumbnail = data['thumbnail']
+                if thumbnail.startswith('/'):
                     thumbnail = thumbnail[1:]
-                if thumbnail.startswith("themes"):
-                    thumbnail = "assets/" + thumbnail
-                if thumbnail.startswith("assets/themes") is False:
+                if thumbnail.startswith('themes'):
+                    thumbnail = 'assets/' + thumbnail
+                if thumbnail.startswith('assets/themes') is False:
                     thumbnail = 'assets/themes/%s/%s' % (dirPath, thumbnail)
-                thumbnail = "/" + thumbnail
+                thumbnail = '/' + thumbnail
                 themes_list.append({
-                    "theme_name": u"" + dirPath,
-                    "theme_title": data["name"] if "name" in data else u"" + dirPath,
-                    "author": data["author"] if "author" in data else u"",
-                    "using": data["using"] if "using" in data else [],
-                    "exclusive": data["exclusive"] if "exclusive" in data else u"all"
+                    'theme_name': u'' + dirPath,
+                    'theme_title': data['name'] if 'name' in data else u'' + dirPath,
+                    'author': data['author'] if 'author' in data else u'',
+                    'using': data['using'] if 'using' in data else [],
+                    'exclusive': data['exclusive'] if 'exclusive' in data else u'all'
                 })
             if len(themes_list) is 0:
                 themes_list = [
-                    {"theme_name": u"default", "theme_title": u"預設樣式", "using":[], "author": u"", "exclusive": u"all"}
+                    {'theme_name': u'default', 'theme_title': u'預設樣式', 'using':[], 'author': u'', 'exclusive': u'all'}
                 ]
             return themes_list
         return get_list()
@@ -160,7 +160,7 @@ class Themes(Controller):
     @route
     def admin_get_files_md5(self):
         from plugins.file.models.file_model import FileModel
-        theme = self.params.get_string("theme")
+        theme = self.params.get_string('theme')
         self.meta.change_view('json')
         model = FileModel
         self.meta.Model= model
@@ -170,8 +170,8 @@ class Themes(Controller):
         scaffold.list(self)
         data_list = []
         for item in self.context[self.scaffold.plural]:
-            data_list.append({"md5": item.last_md5, "path": item.path})
-        self.context["data"] = {
-            "files": data_list,
-            "next": None
+            data_list.append({'md5': item.last_md5, 'path': item.path})
+        self.context['data'] = {
+            'files': data_list,
+            'next': None
         }
--- models/themes_model.py
+++ models/themes_model.py
@@ -12,14 +12,14 @@ from google.appengine.ext import ndb
 
 
 class ThemesModel(BasicModel):
-    theme_title = Fields.StringProperty(required=True, verbose_name=u"樣式名稱")
-    theme_name = Fields.StringProperty(required=True, verbose_name=u"識別名稱")
-    exclusive = Fields.StringProperty(default="all", verbose_name=u"專屬項目")
-    is_enable = Fields.BooleanProperty(default=True, verbose_name=u"顯示於前台")
-    author = Fields.StringProperty(verbose_name=u"作者")
-    using = Fields.TextProperty(verbose_name=u"使用的欄位")
-    thumbnail = Fields.StringProperty(verbose_name=u"縮圖位置")
-    in_datastore = Fields.BooleanProperty(default=False, verbose_name=u"是否位於 DataStore")
+    theme_title = Fields.StringProperty(required=True, verbose_name=u'樣式名稱')
+    theme_name = Fields.StringProperty(required=True, verbose_name=u'識別名稱')
+    exclusive = Fields.StringProperty(default='all', verbose_name=u'專屬項目')
+    is_enable = Fields.BooleanProperty(default=True, verbose_name=u'顯示於前台')
+    author = Fields.StringProperty(verbose_name=u'作者')
+    using = Fields.TextProperty(verbose_name=u'使用的欄位')
+    thumbnail = Fields.StringProperty(verbose_name=u'縮圖位置')
+    in_datastore = Fields.BooleanProperty(default=False, verbose_name=u'是否位於 DataStore')
 
     @classmethod
     def find_by_theme_name(cls, theme_name):
@@ -28,14 +28,14 @@ class ThemesModel(BasicModel):
     @classmethod
     def get_list(cls, identifier_name):
         return cls.query(
-            cls.exclusive.IN([identifier_name, u"all"])
+            cls.exclusive.IN([identifier_name, u'all'])
         ).order(-cls.sort, -cls._key)
 
     @classmethod
     def check_in_list(cls, identifier_name, theme_name):
         item = cls.query(
             ndb.AND(
-                cls.exclusive.IN([identifier_name, u"all"]),
+                cls.exclusive.IN([identifier_name, u'all']),
                 cls.theme_name == theme_name
             )
         ).order(-cls.sort, -cls._key).get()
